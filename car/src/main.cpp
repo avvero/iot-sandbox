41c9089cb76b17eb9d8e04dc9820d6908fe9843f
Антон Беляев <avvero@mts.ru>
2025-10-15 20:51:47 +0700

    Initial commit

---
+++ car/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(13, OUTPUT); // Встроенный светодиод на пине 13
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // Включить светодиод
+  delay(1000);            // Подождать 1 секунду
+  digitalWrite(13, LOW);  // Выключить светодиод
+  delay(1000);            // Подождать 1 секунду
+}
