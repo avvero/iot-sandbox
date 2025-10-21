1ec396793da9d1daf94e66698e8d2ddb878218ee
avvero <avvero@mail.ru>
2025-10-21 13:37:18 +0700

    ir

---
+++ nano-ir/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+
+void setup() {
+  Wire.begin();
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.println("Ready to show....");
+  display.display();
+}
+
+void loop() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.println("Ready to show....");
+  display.display();
+  delay(2000);
+}
